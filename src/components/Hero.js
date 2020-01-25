5c9f8a8d9194ff36875dac2e5a0ab01b7739c8ac
ArturDziadosz <artur.dziadosz1@gmail.com>
2020-01-25 22:46:41 +0100

    0.0.7 Added working heroBanner

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,38 +4,13 @@ import photo1 from '../assets/s1.jpg';
 import photo2 from '../assets/s2.jpg';
 
 class Hero extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      photos: [photo1, photo2, ""],
-      currentPhoto: 0
-    }
-  }
-
-  componentDidMount() {
-    let counter = 0;
-    this.intervalID = setInterval(() => {
-      this.setState({
-        currentPhoto: counter
-      });
-      if (counter === this.state.photos.length-1) {
-        counter = 0;
-      } else {
-        counter++;
-      }
-    }, 4000);
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.intervalID);
-  }
 
   render() {
-    const {photos, currentPhoto} = this.state;
     return (
       <>
         <section className="heroBanner container">
-          <img src={photos[currentPhoto]} alt="Images of flowers" className="heroBanner__img"/>
+          <img src={photo1} alt="Images of flowers" className="heroBanner__img heroBanner__img--first"/>
+          <img src={photo2} alt="Images of flowers" className="heroBanner__img heroBanner__img--second"/>
         </section>
       </>
     )
