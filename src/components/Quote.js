5610e9fa10451a092fe60b091eb5b8e9eeb33ec8
ArturDziadosz <artur.dziadosz1@gmail.com>
2020-01-26 13:36:32 +0100

    0.0.11 Added Quote.js with Quote.scss

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,11 +1,21 @@
 import React, {Component} from 'react';
-import './Header.scss';
+import './Quote.scss';
 
 class Quote extends Component {
   render() {
     return (
       <>
-
+        <article className="quote">
+          <section className="container">
+            <div className="row">
+              <p className="quote__text">"NAJLEPSZYCH I NAJPIĘKNIEJSZYCH RZECZY NA ŚWIECIE, NIE MOŻNA ANI ZOBACZYĆ, ANI DOTKNĄĆ. TRZEBA JE POCZUĆ SERCEM."
+              </p>
+              <p className="quote__author">HELLEN KELLER
+              </p>
+              <div className="quoteButton">ZOBACZ REALIZACJE</div>
+            </div>
+          </section>
+        </article>
       </>
     )
   }
