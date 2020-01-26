95896a018c1edec3a80ad2221e6e5ecf9001f064
ArturDziadosz <artur.dziadosz1@gmail.com>
2020-01-26 13:10:35 +0100

    0.0.10 Added Offer.js with Offer.scss and changed Hero banner displaying

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,7 +1,5 @@
 import React, {Component} from 'react';
 import './Hero.scss';
-import photo1 from '../assets/s1.jpg';
-import photo2 from '../assets/s2.jpg';
 
 class Hero extends Component {
 
@@ -9,8 +7,8 @@ class Hero extends Component {
     return (
       <>
         <section className="heroBanner container">
-          <img src={photo1} alt="Images of flowers" className="heroBanner__img heroBanner__img--first"/>
-          <img src={photo2} alt="Images of flowers" className="heroBanner__img heroBanner__img--second"/>
+          <div className="heroBanner__img heroBanner__img--first"/>
+          <div className="heroBanner__img heroBanner__img--second"/>
         </section>
       </>
     )
--- src/components/Offer.js
+++ src/components/Offer.js
@@ -1,12 +1,31 @@
 import React, {Component} from 'react';
+import './Offer.scss';
 
 class Offer extends Component {
   render() {
     return (
       <>
+        <article className="container">
+          <div className="row">
+            <section className="col-4">
+              <div className="article__img" />
+              <h3 className="article__title">FLORYSTYKA ŚLUBNA</h3>
+              <p className="article__text">Zajmujemy się kompleksową oprawą florystyczną ślubu. Stworzymy dla Was niezapomniane dekoracje w najważniejszym momencie życia.</p>
+            </section>
+            <section className="col-4">
+              <div className="article__img" />
+              <h3 className="article__title">FLORYSTYKA OKOLICZNOŚCIOWA</h3>
+              <p className="article__text">Oferujemy niepowtarzalne aranżacje kwiatowe, pasujące do charakteru i okoliczności przyjęcia. Zadbamy, aby ten dzień był dla Was niezapomniany.</p>
+            </section>
+            <section className="col-4">
+              <div className="article__img" />
+              <h3 className="article__title">FLORYSTYKA DLA DOMU I FIRMY</h3>
+              <p className="article__text">Organizujemy przestrzeń, gdzie nie powinno zabraknąć kwiatów, które wprowadzą wyjątkową atmosferę, świeżość i mnóstwo barw.</p>
+            </section>
+          </div>
+        </article>
       </>
     )
   }
 }
-
 export default Offer;
