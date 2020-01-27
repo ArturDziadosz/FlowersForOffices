74cad23e8d0c27203c8cbf4a22efb54706365419
ArturDziadosz <artur.dziadosz1@gmail.com>
2020-01-27 15:41:05 +0100

    0.0.16 Added Contact.js with Contact.scss

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,28 @@
 import React, {Component} from 'react';
+import "./Contact.scss";
 
 class Contact extends Component {
   render() {
     return (
       <>
+        <div className="contact">
+          <section className="container">
+            <div className="row">
+              <div className="contact__logo"/>
+              <p className="contact__text">Skontaktuj się z nami</p>
+              <div className="contact__mediaBox">
+                <div className="contact__mediaBox__standardMedia">
+                  <p className="standardMedia"><i className="fas fa-mobile-alt"/>+ 48 123 456 789 </p>
+                  <p className="standardMedia"><i className="fas fa-envelope"/>kwiatostan.biuro@gmail.com</p>
+                </div>
+                <div className="contact__mediaBox__socialMedia">
+                  <p className="socialMedia"><i className="fab fa-facebook-square"/></p>
+                  <p className="socialMedia"><i className="fab fa-instagram"/></p>
+                </div>
+              </div>
+            </div>
+          </section>
+        </div>
       </>
     )
   }
