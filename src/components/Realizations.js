dc6c7ac409c910645d6b30acce165325ca6fb339
ArturDziadosz <artur.dziadosz1@gmail.com>
2020-01-28 14:58:16 +0100

    0.1.7 Added nuke-carousel

--- src/components/Realizations.js
+++ src/components/Realizations.js
@@ -1,7 +1,16 @@
 import React, {Component} from 'react';
+import Carousel from 'nuka-carousel';
+import photo1 from '../assets/photobox1.jpg';
+import photo2 from '../assets/photobox2.jpg';
+import photo3 from '../assets/photobox3.jpg';
 import './Realizations.scss';
 
 class Realizations extends Component {
+
+  handleChange = e => {
+    e.currentTarget.style.display = "none";
+  };
+
   render() {
     return (
       <>
@@ -24,14 +33,34 @@ class Realizations extends Component {
             </div>
             <div className="row">
               <div className="realizations__box">
-                <div className="realizations__project">
+                <div className="realizations__project" onClick={this.handleChange}>
                   <i className="fas fa-search"/>
                 </div>
+                <Carousel>
+                  <img src={photo1} alt={"alt"}/>
+                  <img src={photo2} alt={"alt"}/>
+                  <img src={photo3} alt={"alt"}/>
+                </Carousel>
               </div>
               <div className="realizations__box">
                 <div className="realizations__project">
                   <i className="fas fa-search"/>
                 </div>
+              </div>
+
+            </div>
+            <div className="row">
+              <div className="realizations__box">
+                <div className="realizations__project">
+                  <i className="fas fa-search"/>
+                </div>
+              </div>
+              <div className="realizations__box">
+                <Carousel>
+                  <img src={photo1} alt={"alt"}/>
+                  <img src={photo2} alt={"alt"}/>
+                  <img src={photo3} alt={"alt"}/>
+                </Carousel>
               </div>
             </div>
           </section>
