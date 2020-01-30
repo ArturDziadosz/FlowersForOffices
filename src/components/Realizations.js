dc19a58f43a6792b3447efda8210efc35a3a396f
ArturDziadosz <artur.dziadosz1@gmail.com>
2020-01-30 12:14:24 +0100

    0.1.9 Added variable realizations Carousels

--- src/components/Realizations.js
+++ src/components/Realizations.js
@@ -6,8 +6,59 @@ import photo3 from '../assets/photobox3.jpg';
 import './Realizations.scss';
 
 class Realizations extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showCarousel1: false,
+      showCarousel2: false
+    }
+  }
 
-  handleChange = e => {
+  handleChange4 = e => {
+    e.currentTarget.style.visibility = "hidden";
+    e.currentTarget.parentElement.style.flex = "0 1 100%";
+    e.currentTarget.parentElement.style.height = "auto";
+    if (e.currentTarget.parentElement.nextElementSibling !== null) {
+      e.currentTarget.parentElement.nextElementSibling.style.flex = "1 1 50%";
+    } else {
+      e.currentTarget.parentElement.previousElementSibling.style.flex = "1 1 50%";
+    }
+    setTimeout(() => {
+      this.setState({
+        showCarousel2: true
+      })
+    },850);
+  };
+
+  handleChange3 = e => {
+    e.currentTarget.style.visibility = "hidden";
+    e.currentTarget.parentElement.style.flex = "0 1 100%";
+    e.currentTarget.parentElement.style.height = "auto";
+    if (e.currentTarget.parentElement.nextElementSibling !== null) {
+      e.currentTarget.parentElement.nextElementSibling.style.flex = "1 1 50%";
+    } else {
+      e.currentTarget.parentElement.previousElementSibling.style.flex = "1 1 50%";
+    }
+    setTimeout(() => {
+      this.setState({
+        showCarousel1: true
+      })
+    },850);
+  };
+
+  handleChange2 = e => {
+    e.currentTarget.style.visibility = "hidden";
+    e.currentTarget.parentElement.style.flex = "0 1 100%";
+    e.currentTarget.parentElement.style.height = "auto";
+    if (e.currentTarget.parentElement.nextElementSibling !== null) {
+      e.currentTarget.parentElement.nextElementSibling.style.flex = "1 1 50%";
+    } else {
+      e.currentTarget.parentElement.previousElementSibling.style.flex = "1 1 50%";
+    }
+    e.currentTarget.nextElementSibling.style.display = "block";
+  };
+
+  handleChange1 = e => {
     e.currentTarget.style.display = "none";
   };
 
@@ -21,19 +72,7 @@ class Realizations extends Component {
             </div>
             <div className="row">
               <div className="realizations__box">
-                <div className="realizations__project">
-                  <i className="fas fa-search"/>
-                </div>
-              </div>
-              <div className="realizations__box">
-                <div className="realizations__project">
-                  <i className="fas fa-search"/>
-                </div>
-              </div>
-            </div>
-            <div className="row">
-              <div className="realizations__box">
-                <div className="realizations__project" onClick={this.handleChange}>
+                <div className="realizations__project" onClick={this.handleChange1}>
                   <i className="fas fa-search"/>
                 </div>
                 <Carousel>
@@ -43,19 +82,64 @@ class Realizations extends Component {
                 </Carousel>
               </div>
               <div className="realizations__box">
-                <div className="realizations__project">
+                <div className="realizations__project" onClick={this.handleChange1}>
                   <i className="fas fa-search"/>
                 </div>
+                <Carousel>
+                  <img src={photo1} alt={"alt"}/>
+                  <img src={photo2} alt={"alt"}/>
+                  <img src={photo3} alt={"alt"}/>
+                </Carousel>
               </div>
-
             </div>
             <div className="row">
               <div className="realizations__box">
-                <div className="realizations__project">
+                <div className="realizations__project" onClick={this.handleChange3}>
                   <i className="fas fa-search"/>
                 </div>
+                <Carousel style={this.state.showCarousel1 ? {display: "block"} : {display: "none"}}>
+                  <img src={photo1} alt={"alt"}/>
+                  <img src={photo2} alt={"alt"}/>
+                  <img src={photo3} alt={"alt"}/>
+                </Carousel>
+                <div style={this.state.showCarousel1 ? {position: "absolute", zIndex: "-1"} : {}} className={"testing"}/>
               </div>
               <div className="realizations__box">
+                <div className="realizations__project" onClick={this.handleChange4}>
+                  <i className="fas fa-search"/>
+                </div>
+                <Carousel style={this.state.showCarousel2 ? {display: "block"} : {display: "none"}}>
+                  <img src={photo1} alt={"alt"}/>
+                  <img src={photo2} alt={"alt"}/>
+                  <img src={photo3} alt={"alt"}/>
+                </Carousel>
+                <div style={this.state.showCarousel2 ? {position: "absolute", zIndex: "-1"} : {}} className={"testing"}/>
+              </div>
+            </div>
+            <div className="row">
+              <div className="realizations__box">
+                <div className="realizations__project" onClick={this.handleChange2}>
+                  <i className="fas fa-search"/>
+                </div>
+                <Carousel style={{display: "none"}}>
+                  <img src={photo1} alt={"alt"}/>
+                  <img src={photo2} alt={"alt"}/>
+                  <img src={photo3} alt={"alt"}/>
+                </Carousel>
+              </div>
+              <div className="realizations__box">
+                <div className="realizations__project" onClick={this.handleChange2}>
+                  <i className="fas fa-search"/>
+                </div>
+                <Carousel style={{display: "none"}}>
+                  <img src={photo1} alt={"alt"}/>
+                  <img src={photo2} alt={"alt"}/>
+                  <img src={photo3} alt={"alt"}/>
+                </Carousel>
+              </div>
+            </div>
+            <div className="row">
+              <div className="realizations__box" style={{height: "auto"}}>
                 <Carousel>
                   <img src={photo1} alt={"alt"}/>
                   <img src={photo2} alt={"alt"}/>
