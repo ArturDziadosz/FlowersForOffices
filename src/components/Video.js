b7f4be27203e26104ab001f4c9de1daa6772378d
ArturDziadosz <artur.dziadosz1@gmail.com>
2020-07-11 21:51:30 +0200

    0.5.3 added subtitles and logo

--- src/components/Video.js
+++ src/components/Video.js
@@ -8,11 +8,13 @@ class Video extends Component {
         <article className="video">
           <section className="container">
             <div className="row">
-              <p className="realizations__title">KONIECZNIE ZOBACZ</p>
+              <div className="video__img"/>
+              <p className="realizations__title">TWOJA WŁASNA ANTYSMOGOWA OCHRONA</p>
             </div>
             <div className={"video__frame"} style={{margin: "0 auto"}}>
               <div style={{position: "relative", height: "0", paddingBottom: "56.25%"}}>
-                <iframe src={"https://embed.ted.com/talks/kamal_meattle_how_to_grow_fresh_air"} width={"854"}
+                <iframe src={"https://embed.ted.com/talks/kamal_meattle_how_to_grow_fresh_air?texttrack=pl"}
+                        width={"854"}
                         height={"480"}
                         style={{position: "absolute", left: "0", top: "0", width: "100%", height: "100%"}}
                         frameBorder={"0"}
