ab126c234646543dea202e7e4851b591e451c6b7
ArturDziadosz <artur.dziadosz1@gmail.com>
2020-08-02 21:38:15 +0200

    0.5.6 changed offset + added top position to hamburger menu

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -35,19 +35,29 @@ class NavBar extends Component {
   render() {
     return (
       <>
-        <nav className={this.state.hamburgerMenuOpen ? "nav nav--hamburger" : "nav"}>
+        <nav className={this.state.hamburgerMenuOpen ? "nav nav--hamburger" : "nav"}
+             style={this.state.topPosition ? {top: "120px"} : {top: "58px"}}>
           <ul className={"nav__list"}>
-            <li className={this.state.topPosition ? "nav__list__elem" : "nav__list__elem nav__list__elem--short"}><Link to={"header"} spy={true} smooth={true} duration={1000} offset={-250} onClick={this.handleCloseHamburgerMenu}>STRONA GŁÓWNA</Link></li>
-            <li className={this.state.topPosition ? "nav__list__elem" : "nav__list__elem nav__list__elem--short"} /*onMouseEnter={this.handleExtraMenu} onMouseLeave={this.handleExtraMenu}*/ ><Link to={"offer"} spy={true} smooth={true} duration={1000} offset={-140} onClick={this.handleCloseHamburgerMenu}>OFERTA {/*<i className="fas fa-chevron-down" />*/}</Link>
-{/*              <ul className={"nav__list__more"} style={this.state.extraMenu ? {display: "block"} : {}}>
+            <li className={this.state.topPosition ? "nav__list__elem" : "nav__list__elem nav__list__elem--short"}><Link
+              to={"header"} spy={true} smooth={true} duration={1000} offset={-250}
+              onClick={this.handleCloseHamburgerMenu}>STRONA GŁÓWNA</Link></li>
+            <li
+              className={this.state.topPosition ? "nav__list__elem" : "nav__list__elem nav__list__elem--short"} /*onMouseEnter={this.handleExtraMenu} onMouseLeave={this.handleExtraMenu}*/ >
+              <Link to={"offer"} spy={true} smooth={true} duration={1000} offset={-50}
+                    onClick={this.handleCloseHamburgerMenu}>OFERTA {/*<i className="fas fa-chevron-down" />*/}</Link>
+              {/*              <ul className={"nav__list__more"} style={this.state.extraMenu ? {display: "block"} : {}}>
                 <li className="nav__list__more__child">FLORYSTYKA ŚLUBNA</li>
                 <li className="nav__list__more__child">
                   FLORYSTYKA OKOLICZNOŚCIOWA</li>
                 <li className="nav__list__more__child">FLORYSTYKA DLA DOMU I FIRMY</li>
               </ul>*/}
             </li>
-            <li className={this.state.topPosition ? "nav__list__elem" : "nav__list__elem nav__list__elem--short"} ><Link to={"realization"} spy={true} smooth={true} duration={1000} offset={-130} onClick={this.handleCloseHamburgerMenu}>REALIZACJE</Link></li>
-            <li className={this.state.topPosition ? "nav__list__elem" : "nav__list__elem nav__list__elem--short"} ><Link to={"contact"} spy={true} smooth={true} duration={1000} onClick={this.handleCloseHamburgerMenu}>KONTAKT</Link></li>
+            <li className={this.state.topPosition ? "nav__list__elem" : "nav__list__elem nav__list__elem--short"}><Link
+              to={"realization"} spy={true} smooth={true} duration={1000} offset={-80}
+              onClick={this.handleCloseHamburgerMenu}>REALIZACJE</Link></li>
+            <li className={this.state.topPosition ? "nav__list__elem" : "nav__list__elem nav__list__elem--short"}><Link
+              to={"contact"} spy={true} smooth={true} duration={1000}
+              onClick={this.handleCloseHamburgerMenu}>KONTAKT</Link></li>
           </ul>
         </nav>
       </>
